268167493ee2eabab4e8250d60ca13335da52630
Nandiehakuna <gakiikelly403@gmail.com>
2024-03-15 17:55:36 +0300

    chapter 1 partially completed

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,11 @@
+  const toggleBtn = document.querySelector(".toggle-btn");
+       const dropdown =document.querySelector(".dropdown");
+
+       const menu=document.querySelector(".menu");
+
+
+        toggleBtn.addEventListener("click", ()=>{
+                dropdown.classList.toggle("open");
+
+
+            });
